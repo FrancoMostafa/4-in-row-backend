a9265cfa6b12d9693e74c4d5003ba07e5bccdb88
FrancoMostafa <francom2342@gmail.com>
2022-11-14 20:50:03 -0300

    cors

--- src/main/java/inrowbackend/controller/StatisticsController.java
+++ src/main/java/inrowbackend/controller/StatisticsController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,18 +21,21 @@ public class StatisticsController {
 	@Autowired
 	private StatisticsService statisticsService;
 	
+	@CrossOrigin(origins = "${origin_frontend}")
 	@RequestMapping(value = "", method = RequestMethod.GET)
 	public ResponseEntity<List<StatisticsModel>> getStatistics() {
 		List<StatisticsModel> response = statisticsService.getStatistics();
 		return new ResponseEntity<List<StatisticsModel>>(response, HttpStatus.OK);
 	}
 	
+	@CrossOrigin(origins = "${origin_frontend}")
 	@RequestMapping(value = "/get/{day}/{month}/{year}", method = RequestMethod.POST)
 	public ResponseEntity<StatisticsModel> getStatisticsByDate(@PathVariable Integer day,@PathVariable  Integer month, @PathVariable Integer year) {
 		StatisticsModel response = statisticsService.getStatisticsByDate(day, month, year);
 		return new ResponseEntity<StatisticsModel>(response, HttpStatus.OK);
 	}
 	
+	@CrossOrigin(origins = "${origin_frontend}")
 	@RequestMapping(value = "/{gameId}/{type}/{state}/{playerCountry}", method = RequestMethod.POST)
 	public ResponseEntity<StatisticsModel> addStatisticsData(@PathVariable String gameId, @PathVariable String type, @PathVariable String state , @PathVariable String playerCountry) {
 		String countryResult = this.getCountryResult(playerCountry);
